f91a92b7b3c82dd8fa0fa02f7c625da4bc0d1936
drunkz <drunkz@163.com>
2020-11-10 00:16:11 +0800

    空工程

---
+++ Source/ElyClient/ElyClient.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ElyClient.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ElyClient, "ElyClient" );
---
+++ Source/ElyClient/ElyClientGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ElyClientGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELYCLIENT_API AElyClientGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
